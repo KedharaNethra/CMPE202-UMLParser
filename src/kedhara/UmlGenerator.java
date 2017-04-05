564581b41097f76cbb61a4394a5a5eb6e113b884
KedharaNethra <kedharanethra.thiruvuru@sjsu.edu>
2017-04-04 23:53:36 -0700

    Modified URLConnection code

--- src/kedhara/UmlGenerator.java
+++ src/kedhara/UmlGenerator.java
@@ -2,31 +2,38 @@ package kedhara;
 
 import java.io.*;
 import java.net.*;
+import java.io.FileOutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.sun.org.apache.xerces.internal.util.URI.MalformedURIException;
 
 public class UmlGenerator {
 	public static Boolean generatePNG(String grmr, String oPath) {
 
         try {
-            String webLink = "https://yuml.me/diagram/plain/class/" + grmr
+            String yumlLink = "cons://yuml.me/diagram/plain/class/" + grmr
                     + ".png";
-            URL url = new URL(webLink);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Accept", "application/json");
+            URL url = new URL(yumlLink);
+            HttpURLconnection ucon = (HttpURLconnection) url.openConnection();
+            ucon.setRequestMethod("GET");
+            ucon.setRequestProperty("Accept", "application/json");
+    		System.out.println(ucon.getResponseCode());
 
-            if (con.getResponseCode() != 200) {
+            if (ucon.getResponseCode() != 200) {
                 throw new RuntimeException(
-                        "Connection Failed : Error code is : " + con.getResponseCode());
+                        "connection Failed : Error code is : " + ucon.getResponseCode());
             }
             OutputStream oStream = new FileOutputStream(new File(oPath));
             int r = 0;
             byte[] bytes = new byte[1024];
 
-            while ((r = con.getInputStream().read(bytes)) != -1) {
+            while ((r = ucon.getInputStream().read(bytes)) != -1) {
                 oStream.write(bytes, 0, r);
             }
             oStream.close();
-            con.disconnect();
+            ucon.disconnect();
         } catch (MalformedURLException uexp) {
             uexp.printStackTrace();
         } catch (IOException uexp) {
