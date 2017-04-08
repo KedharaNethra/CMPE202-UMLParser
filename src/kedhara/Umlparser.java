05354a9d375f78d76b57ff0f168084cb352a0f1f
KedharaNethra <kedharanethra.thiruvuru@sjsu.edu>
2017-04-07 18:17:53 -0700

    Added code to call and generate sequence based on arguements

--- src/kedhara/Umlparser.java
+++ src/kedhara/Umlparser.java
@@ -4,11 +4,18 @@ public class Umlparser {
 
     public static void main(String[] args) throws Exception {
         if (args[0].equals("class")) {
-            StartToParse stp = new StartToParse(args[1], args[2]);
-            stp.start();
-               } else {
+        	System.out.println("Entered class");
+            StartToParse sp = new StartToParse(args[1], args[2]);
+            sp.start();
+        }else if (args[0].equals(("sequence"))){
+            System.out.println("Entered Sequence");
+        	ParseSeqCode  seq = new ParseSeqCode(args[1],args[2],args[3],args[4]);
+            seq.start();
+        }
+            
+        else
+        {
             System.out.println("Arguement is invalid " + args[0]);
-         //Add code to call Sequence   
         }
 
     }
