5905075654284bb70ab25c22560da6e7c978bd06
KedharaNethra <kedharanethra.thiruvuru@sjsu.edu>
2017-04-22 23:23:20 -0700

    Removed sequence- will add as seperate

--- src/kedhara/Umlparser.java
+++ src/kedhara/Umlparser.java
@@ -4,14 +4,15 @@ public class Umlparser {
 
     public static void main(String[] args) throws Exception {
         if (args[0].equals("class")) {
-        	System.out.println("Entered class");
+        	System.out.println("Generate class");
             StartToParse sp = new StartToParse(args[1], args[2]);
             sp.start();
-        }else if (args[0].equals(("sequence"))){
-            System.out.println("Entered Sequence");
-        	ParseSeqCode  seq = new ParseSeqCode(args[1],args[2],args[3],args[4]);
-            seq.start();
         }
+ //       else if (args[0].equals(("sequence"))){
+ ///           System.out.println("Entered Sequence");
+  //      	ParseSeqCode  seq = new ParseSeqCode(args[1],args[2],args[3],args[4]);
+  //          seq.start();
+  //      }
             
         else
         {
